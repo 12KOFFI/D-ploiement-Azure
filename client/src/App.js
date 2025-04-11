9e2d813ba72a06a1137dffd46d7d7661f5c3e14d
Isaac <isaacndri5@gmail.com>
2025-04-11 12:42:45 +0000

    nouveau

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,48 @@
 import React from "react";
 import axios from 'axios';
-import baseUrl from './baseUrl'; // Importez votre fichier de configuration
 import "./index.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      bestShows: {} 
+      bestShows: {} // ← initialise avec un objet, pas un tableau
     };
   }
 
   componentDidMount() {
-    console.log("Requête envoyée à :", baseUrl); // Vérifiez l'URL dans la console
-    axios.get(`${baseUrl}/api/data`) // Utilisez baseUrl ici
+    console.log("componentDidMount success");
+    axios.get('/api/data')
       .then(res => {
-        console.log("Données reçues :", res.data);
+        console.log("data recieved: ", res.data);
         const firstItem = (res.data && res.data.length > 0) ? res.data[0] : {};
         this.setState({ bestShows: firstItem });
       })
       .catch(err => {
         alert("Erreur lors de la récupération des données");
-        console.error("Détails de l'erreur :", err.response || err.message);
+        console.error(err);
       });
   }
 
   render() {
     const { bestShows } = this.state;
-    const isValidData = bestShows && typeof bestShows === "object" && Object.keys(bestShows).length > 0;
+    console.log("render bestShows: ", bestShows);
+
+    const isValidData = bestShows && typeof bestShows === "object";
 
     return (
-      <div className="app-container">
+      <div>
         <h1>azure-mern-demo</h1>
-        <ul className="data-list">
-          {isValidData ? (
-            Object.entries(bestShows).map(([key, value], idx) => (
-              <li key={idx}>
-                <strong>{key}</strong>: {value}
-              </li>
-            ))
-          ) : (
-            <li className="no-data">Chargement des données...</li>
-          )}
+        <ul>
+          {
+            isValidData && Object.keys(bestShows).length > 0 ? (
+              Object.keys(bestShows).map((cur, idx) => (
+                <li key={idx}>{cur} - {bestShows[cur]}</li>
+              ))
+            ) : (
+              <li>No data available</li>
+            )
+          }
         </ul>
       </div>
     );
